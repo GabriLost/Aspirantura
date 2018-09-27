d339cf0588566f40715c24f6a0ccb631597b4645
GabriLost <parannoic@gmail.com>
2018-09-28 00:01:58 +0300

    почему-то заработало
    12

--- PingServicePlugin-master/src/main/java/exception/RequiredComponentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-//package exception;
-//
-//public class RequiredComponentNotFoundException extends Exception {
-//    public RequiredComponentNotFoundException(Object userInfoManagerClass) {
-//        super("No " + userInfoManagerClass.toString());
-//    }
-//}
--- PingServicePlugin-master/src/main/java/ru/gabri/UploadService.java
+++ PingServicePlugin-master/src/main/java/ru/gabri/UploadService.java
@@ -1,12 +1,5 @@
 package ru.gabri;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import com.google.gson.*;
 import com.atlassian.configurable.ObjectConfiguration;
 import com.atlassian.configurable.ObjectConfigurationException;
 import com.atlassian.jira.bc.issue.search.SearchService;
@@ -18,21 +11,26 @@ import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.service.AbstractService;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.web.bean.PagerFilter;
+import com.google.gson.Gson;
 import com.opensymphony.module.propertyset.PropertySet;
-import ru.gabri.dao.LastRun;
-import ru.gabri.dao.LastRunStorage;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class UploadService extends AbstractService{
     private String jqlQuery, user, path, date = null;
     private String cong_elem_field_id;
     private String bug_stage_field_id;
-    private LastRun lastRun;
+//    private LastRun lastRun;
 
-    public UploadService( LastRunStorage lastRun){
-        this.lastRun = lastRun;
-    }
+//    public UploadService( LastRunStorage lastRun){
+//        this.lastRun = lastRun;
+//    }
 
     @Override
     public void init(PropertySet props) throws ObjectConfigurationException {
@@ -46,8 +44,8 @@ public class UploadService extends AbstractService{
     }
     public void run() {
 
-        this.lastRun.setValue("key", "WOW SO MUCH BIG TEXT SEE HERE MOTHER FUCKER");
-        System.out.println(lastRun.getValue("key"));
+//        this.lastRun.setValue("key", "WOW SO MUCH BIG TEXT SEE HERE MOTHER FUCKER");
+//        System.out.println(lastRun.getValue("key"));
 
         System.out.println("_"+jqlQuery);
         System.out.println("_"+user);
@@ -91,7 +89,7 @@ public class UploadService extends AbstractService{
     private List<Issue> parseJQL(String JQL) {
         ApplicationUser applicationUser = setAppUser(user);
         SearchService searchService = ComponentAccessor.getComponentOfType(SearchService.class);
-        final SearchService.ParseResult parseResult = searchService.parseQuery(applicationUser, jqlQuery);
+        final SearchService.ParseResult parseResult = searchService.parseQuery(applicationUser, JQL);
 
         if (parseResult.isValid()) {
             try
